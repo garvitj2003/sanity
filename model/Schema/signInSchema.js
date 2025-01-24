ddf0bdf2fddc7de78da1edaa64bffb5db0cb5380
JKrishnaD <jayak5063@gmail.com>
2025-01-24 11:10:40 +0530

    added input validation

--- model/Schema/signInSchema.js
+++ model/Schema/signInSchema.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  email: z.string(),
-  password: z.string(),
+  email: z.string().email("Enter valid Email"),
+  password: z.string().min(6),
 });
--- model/Schema/signUpSchema.js
+++ model/Schema/signUpSchema.js
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 export const usernameValidation = z
   .string()
-  .min(2, "Username must be at least 2 characters")
-  .max(20, "Username must be no more than 20 characters")
-  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters");
+  .min(3, { message: "Username must be at least 3 characters" })
+  .max(20, { message: "Username must be no more than 20 characters" })
+  .regex(/^[a-zA-Z0-9_]+$/, {
+    message: "Username must not contain special characters",
+  });
 
 export const signUpSchema = z.object({
   username: usernameValidation,
