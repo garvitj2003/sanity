b2bf8b5d635baca81c5d0da8e33185e24ff1a7f1
Kuzuri247 <mindsetmatters47@gmail.com>
2025-01-31 23:49:31 +0530

    some changes in landing

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AuthLayout({ children }) {
   return (
     <div>
-      <div className="flex border h-[90vh] -mt-8 w-[98%] mx-auto rounded-2xl  border-zinc-200/20 ">
+      <div className="flex border h-[90vh] -mt-8 w-[98%] mx-auto rounded-2xl border-zinc-200/20 ">
         <div className="w-1/2 relative p-2 flex items-center justify-center max-md:hidden">
           <div className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center relative z-20 pb-2 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
             Welcome to Sanity
